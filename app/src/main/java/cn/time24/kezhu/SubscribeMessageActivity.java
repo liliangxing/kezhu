5024f60b4012939d2a3ee8ba52891238041689cb
liliangxing <253254457@qq.com>
2020-03-11 11:36:54 +0800

    1

--- app/src/main/java/cn/time24/kezhu/SubscribeMessageActivity.java
+++ app/src/main/java/cn/time24/kezhu/SubscribeMessageActivity.java
@@ -274,27 +274,4 @@ public class SubscribeMessageActivity extends Activity implements View.OnClickLi
 		}
 		return super.onKeyDown(keyCode, event);//返回键的super处理的就是退出应用
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			doFinish();
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
 }
